5087cd125bce193e2fa2b0be51d687cee270445a
Frata Mamud <e2395117@cmaisonneuve.qc.ca>
2024-01-24 14:28:45 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// criar um servidor
+const http = require("http");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+// Au debut du fichier
+dotenv.config();
+
+const server = http.createServer((request, response) => {
+    if (request.method == "GET" && request.url == "/") {
+        const file = fs.readFileSync('./public/index.html', 'utf-8');
+
+        response.setHeader('Content-Type', 'text.html');
+        response.statusCode = 200;
+
+        response.end(file);
+    } else {
+        const file = fs.readFileSync('./public/404.html', 'utf-8');
+
+        response.setHeader('Content-Type', 'text.html');
+        response.statusCode = 404;
+
+        response.end(file);
+    }
+});
+
+server.listen(process.env.PORT, () => {
+    console.log("Le serveur a démarré.")
+});
+
